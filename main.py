4b98dfddb96dab40be1c1ef4f5d9117d4e94d370
rubaiiyat <abirr1516@gmail.com>
2026-08-22 23:08:49 +0600

    return json file

--- main.py
+++ main.py
@@ -2,6 +2,42 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
+users=[
+  {
+    "id": 1,
+    "name": "Abir",
+    "age": 24,
+    "is_student": True,
+    "skills": ["Python", "FastAPI", "Django"],
+    "address": {
+      "city": "Dhaka",
+      "country": "Bangladesh"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Rahim",
+    "age": 25,
+    "is_student": False,
+    "skills": ["JavaScript", "React", "Node.js"],
+    "address": {
+      "city": "Chittagong",
+      "country": "Bangladesh"
+    }
+  },
+  {
+    "id": 3,
+    "name": "Karim",
+    "age": 23,
+    "is_student": True,
+    "skills": ["C++", "Python", "SQL"],
+    "address": {
+      "city": "Rajshahi",
+      "country": "Bangladesh"
+    }
+  }
+]
+
 @app.get('/')
 async def home():
     return {'message':'Fastapi is running'}
@@ -9,3 +45,7 @@ async def home():
 @app.get('/name')
 async def user(name:str):
     return (f'Hello {name}')
+
+""" @app.get('/users')
+async def get_users():
+    return users """
